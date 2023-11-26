06cd1dbdd83574e78e2d3d529b80800984c39070
Alex Voss <alex@corealisation.com>
2023-11-26 16:46:15 -0500

    fix: exception on incomplete .example.yml, added to mkdocs.yml
    code maintenance:
    - added missing encoding to open (PyLint W1514)
    - removed f-strings from log statements (PyLint W1309)
    - reorganized imports (PyLint misc)
    - removed use of global variable (PyLint W0603)
    - avoiding use of 'type' as a variable name

--- hooks/index_pages.py
+++ hooks/index_pages.py
@@ -19,17 +19,13 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 
-import inspect
 import logging
 import os
 import re
-import yaml
-
 from glob import iglob
+
+import yaml
 from mkdocs.config.defaults import MkDocsConfig
-from mkdocs.structure.files import File, Files
-from mkdocs.structure.pages import Page
-from re import Match
 
 # -----------------------------------------------------------------------------
 # State
@@ -47,17 +43,15 @@ def on_pre_build(config: MkDocsConfig):
     Populate the module variable examples with the data from all the
     `.example.y(a)ml` files we can find in `examples/`.
     """
-    global examples
-    examples = []
 
     # Create archives for each example
     ymlfiles = iglob("examples/*/.example.y*ml", recursive = True)
     for file in ymlfiles:
-        with open(file, 'r') as f:
+        with open(file, 'r', encoding='utf-8') as f:
             example = yaml.safe_load(f)['example']
             example['path'] = os.path.dirname(file)
             examples.append(example)
-    log.info(f"Found {len(examples)} examples with metadata.")
+    log.info("Found %d examples with metadata.", len(examples))
 
 def on_page_markdown(markdown: str, page, config, files):
     """
@@ -65,22 +59,31 @@ def on_page_markdown(markdown: str, page, config, files):
     replace it with the list of examples.
     """
 
-    def replace(match: Match):
+    def replace(match: re.Match):
         rendered = []
-        type, args = match.groups()
+        indextype, args = match.groups()
         args = args.strip()
-        if type == "all":
+        if indextype == "all":
             for example in examples:
                 rendered.append(render(example))
             return '\n'.join(rendered)
+        else:
+            log.error("At the moment, we do not support filtering, so use 'all'.")
+            return "Invalid index placeholder."
 
-    log.info(f"looking for index definitions")
+    log.info("looking for index definitions")
     return re.sub(
         r"<!-- index:(\w+)(.*?) -->",
         replace, markdown, flags = re.I | re.M
     )
 
 def render(example: dict) -> str:
+    """
+    Render an individual example and return the HTML as a string.
+    """
+    if not 'name' in example and 'path' in example:
+        log.info("ignored example %s as it was missing a name", example['path'])
+        return ""
     result = f""" <div class="admonition note">
     <div style="float:right;padding-left:1em">{render_tags(example['tags'])}</div>
     <a href="/{example['path']}">
@@ -90,6 +93,9 @@ def render(example: dict) -> str:
     return result
 
 def render_tags(tags: list) -> str:
+    """
+    Render a list of tags. TODO: discuss how tags can be selected?
+    """
     return ', '.join(tags)
 
 # -----------------------------------------------------------------------------
